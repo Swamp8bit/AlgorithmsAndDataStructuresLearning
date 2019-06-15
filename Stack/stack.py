9b5f0366c2b6a10842565b24fd04d7e690e1a5d5
Shapovalov <swamp.8bit@ingrammicro.com>
2019-06-15 20:46:27 +0300

    Create stack.py
    Initial commit for stack realization

---
+++ Stack/stack.py
@@ -0,0 +1,14 @@
+class Stack:
+    def __init__(self):
+        self.stack=[]
+    
+    def size(self):
+        return len(self.stack)
+    
+    def pop(self):
+        return None #if the stack is empty
+
+    def push(self, value):
+
+    def peek(self):
+        return None
