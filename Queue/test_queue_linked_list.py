d15f0864a411bf37db4cf1af5f3aba57779cc51b
Evgeny S <eshapovalovdev@gmail.com>
2019-06-27 21:25:49 +0300

    check lot of size

--- Queue/test_queue_linked_list.py
+++ Queue/test_queue_linked_list.py
@@ -6,6 +6,7 @@ class TestQueue(unittest.TestCase):
     def test_queue_empty_list(self):
         q=Queue()
         self.assertIsNone(q.dequeue(), "It should be None")
+        self.assertEqual(q.size(),0)
     
     def test_dequeue_default(self):
         q=Queue()
@@ -19,4 +20,26 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(q.dequeue(), 1000, "dequeue 1000" )
         self.assertEqual(q.dequeue(), -123, "dequeue -123" )
         self.assertEqual(q.size(),0, "the size is 0")
+
+    def test_lot_of_enqueue(self):
+        q=Queue()
+        length=1000
+        for i in range(0,length):
+            q.enqueue(i)
+        self.assertEqual(q.size(),length,f"The size is {length}")
+
+
+    def test_lof_of_dequeue(self):
+        q=Queue()
+        length=1000
+        for i in range(0,length):
+            q.enqueue(i)
+
+        self.assertEqual(q.size(),length,f"The size is {length}")
+        for i in range(0,length):
+            check=q.dequeue()
+            self.assertEqual(check, i, f"dequeue is {i}")
+            check_size=length-i-1
+            self.assertEqual(check_size, q.size(), f"size is {check_size}")
+
 
