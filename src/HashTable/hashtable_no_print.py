fcc5855e6b216e36f76e8bb8db84463264831762
Evgeny S <eshapovalovdev@gmail.com>
2019-08-11 12:41:18 +0200

    Reformat code

--- src/HashTable/hashtable_no_print.py
+++ src/HashTable/hashtable_no_print.py
@@ -5,41 +5,38 @@ class HashTable:
         self.slots = [None] * self.size
 
     def hash_fun(self, value):
-        sum=0
+        result = 0
         for pos in range(len(value)):
-            sym=value[pos]
-            sum+=ord(sym)*pos
-        return sum%self.size
+            sym = value[pos]
+            result += ord(sym) * pos
+        return result % self.size
 
     def seek_slot(self, value):
-        slot=self.hash_fun(value)
-        iteration=0
+        slot = self.hash_fun(value)
+        iteration = 0
         while iteration < self.size:
-            iteration+=1
+            iteration += 1
             if self.slots[slot] is None:
                 return slot
             else:
-                slot=(slot+self.step)%self.size
+                slot = (slot + self.step) % self.size
         return None
 
-
     def put(self, value):
-
-        slot=self.seek_slot(value)
+        slot = self.seek_slot(value)
         if slot is not None:
-            self.slots[slot]=value
+            self.slots[slot] = value
             return slot
         else:
             return None
 
     def find(self, value):
-        # находит индекс слота со значением, или None
-        slot=self.hash_fun(value)
+        slot = self.hash_fun(value)
         iteration = 0
         while iteration < self.size:
             iteration += 1
             if self.slots[slot] == value:
                 return slot
             else:
-                slot=(slot+self.step)%self.size
+                slot = (slot + self.step) % self.size
         return None
