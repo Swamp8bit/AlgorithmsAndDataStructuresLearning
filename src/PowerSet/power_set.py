43c48bec9231e4c20118cd19eb4730a96a2924b7
Evgenys Shapovalov <swamp.8bit@gmail.com>
2020-09-22 14:37:19 +0300

    Fix bugs in powerset, add more tests

--- src/PowerSet/power_set.py
+++ src/PowerSet/power_set.py
@@ -96,9 +96,14 @@ class PowerSet:
 
     def union(self, set2):
         # объединенbе текущего множества и set2
+        if self.size()==0:
+            return set2
+        if set2.size()==0:
+            return self
         new_sz=set2.sz+self.sz
         set1_only_values = self.only_values()
         set2_only_values=set2.only_values()
+
         result=PowerSet(new_sz)
         for value in set1_only_values:
             result.put(value)
@@ -108,8 +113,12 @@ class PowerSet:
 
     def difference(self, set2):
         # разница текущего множества и set2
-        result=PowerSet(self.sz)
+
+        if self.size() == 0:
+            return self
+        result = PowerSet(self.sz)
         set1_only_values = self.only_values()
+
         for value in set1_only_values:
             if set2.get(value):
                 continue
@@ -126,10 +135,11 @@ class PowerSet:
             if not self.get(value):
                 return False
         return True
-
+#
 # from time import perf_counter
-#
-#
+# import urllib3
+# #
+# #
 # if __name__ == '__main__':
 #     S1=PowerSet(23)
 #     assert S1.size() == 0
@@ -147,31 +157,70 @@ class PowerSet:
 #     print([x for x in S1.elements if x is not None])
 #
 #     string1='ABCDEF'
-#     string2='YQWRTUEF'
-#     S2 = PowerSet(23)
+#     string2=''
 #     S3 = PowerSet(23)
+#     S2 = PowerSet(23)
 #     for s in string1:
 #         S2.put(s)
 #     for s in string2:
 #         S3.put(s)
-#     # start = perf_counter()
-#     # S4=S2.intersection(S3)
-#     # print(S4.only_values())
-#     # end = perf_counter()
-#     # print(end-start)
-#
-#     # start = perf_counter()
-#     # S4 = S2.union(S3)
-#     # print(S3.only_values())
-#     # print(S2.only_values())
-#     # print(S4.only_values())
-#     # end = perf_counter()
-#     # print(end - start)
-#
+# #     # start = perf_counter()
+# #     # S4=S2.intersection(S3)
+# #     # print(S4.only_values())
+# #     # end = perf_counter()
+# #     # print(end-start)
+# #
 #     start = perf_counter()
-#     S4 = S2.difference(S3)
+#     S4 = S2.union(S3)
 #     print(S3.only_values())
 #     print(S2.only_values())
 #     print(S4.only_values())
 #     end = perf_counter()
 #     print(end - start)
+# #
+#     start = perf_counter()
+#     S4 = S3.difference(S2)
+#     print(S3.only_values())
+#     print(S2.only_values())
+#     print(S4.only_values())
+#     end = perf_counter()
+#     print(end - start)
+#
+#     string4='ABCDEF'
+#     string5='ABC'
+#     S4 = PowerSet(23)
+#     S5 = PowerSet(23)
+#     for s in string4:
+#         S4.put(s)
+#     for s in string5:
+#         S5.put(s)
+#
+#     assert S4.issubset(S5) == True
+#     assert S5.issubset(S4) == False
+#
+#         S1=PowerSet(20023)
+#         S2=PowerSet(20023)
+#         http = urllib3.PoolManager()
+#         r = http.request('GET', 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt')
+#         list_of_words=r.data.decode('utf-8').split('\n')
+#         limit=20000
+#         for w in list_of_words:
+#             if limit < 0:
+#                 break
+#             if limit < 10000:
+#                 S1.put(w)
+#             else:
+#                 S2.put(w)
+#             limit-=1
+#
+#         print(S1.only_values())
+#         print(S2.only_values())
+#
+#         start = perf_counter()
+#         S3 = S1.union(S2)
+#         end = perf_counter()
+#         time_perf=end-start
+#         print(f"Union time is {time_perf}")
+#         print(f"The size of S3 is {S3.size()}")
+#         print(f"The size of S1 is {S1.size()}")
+#         print(f"The size of S2 is {S2.size()}")
