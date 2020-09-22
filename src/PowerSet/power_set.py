1101c069f4e404bc736e213904001d57cacd7291
Evgenys Shapovalov <swamp.8bit@gmail.com>
2020-09-22 19:00:09 +0300

    Comment tests

--- src/PowerSet/power_set.py
+++ src/PowerSet/power_set.py
@@ -90,82 +90,82 @@ class PowerSet:
                 return False
         return True
 
-from time import perf_counter
-import urllib3
+# from time import perf_counter
+# import urllib3
+# #
+# #
+# if __name__ == '__main__':
+#     S1=PowerSet()
+#     assert S1.size() == 0
+#     S1.put('AhalayMahalay')
+#     assert S1.get('AhalayMahalay') == True
+#     S1.put('AhalayMahalay')
+#     assert S1.size() == 1
+#     assert S1.size() == 1
+#     S1.put('SimSalavim')
+#     assert S1.get('SimSalavim') == True
+#     assert S1.size() == 2
+#     assert S1.remove('SimSalavim') == True
+#     assert S1.size() == 1
+#     print(S1.elements)
+#     print([x for x in S1.elements if x is not None])
+#
+#     string1='ABCDEF'
+#     string2=''
+#     S3 = PowerSet()
+#     S2 = PowerSet()
+#     for s in string1:
+#         S2.put(s)
+#     for s in string2:
+#         S3.put(s)
+#
+#     start = perf_counter()
+#     S4 = S2.union(S3)
+#
+#     end = perf_counter()
+#     print(end - start)
+# #
+#     start = perf_counter()
+#     S4 = S3.difference(S2)
+#
+#     end = perf_counter()
+#     print(end - start)
+#
+#     string4='ABCDEF'
+#     string5='ABC'
+#     S4 = PowerSet()
+#     S5 = PowerSet()
+#     for s in string4:
+#         S4.put(s)
+#     for s in string5:
+#         S5.put(s)
+#
+#     assert S4.issubset(S5) == True
+#     assert S5.issubset(S4) == False
+#
+#     S1=PowerSet()
+#     S2=PowerSet()
+#     http = urllib3.PoolManager()
+#     r = http.request('GET', 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt')
+#     list_of_words=r.data.decode('utf-8').split('\n')
+#     limit=20000
+#     for w in list_of_words:
+#         if limit < 0:
+#             break
+#         if limit < 10000:
+#             S1.put(w)
+#         else:
+#             S2.put(w)
+#         limit-=1
 #
 #
-if __name__ == '__main__':
-    S1=PowerSet()
-    assert S1.size() == 0
-    S1.put('AhalayMahalay')
-    assert S1.get('AhalayMahalay') == True
-    S1.put('AhalayMahalay')
-    assert S1.size() == 1
-    assert S1.size() == 1
-    S1.put('SimSalavim')
-    assert S1.get('SimSalavim') == True
-    assert S1.size() == 2
-    assert S1.remove('SimSalavim') == True
-    assert S1.size() == 1
-    print(S1.elements)
-    print([x for x in S1.elements if x is not None])
-
-    string1='ABCDEF'
-    string2=''
-    S3 = PowerSet()
-    S2 = PowerSet()
-    for s in string1:
-        S2.put(s)
-    for s in string2:
-        S3.put(s)
-
-    start = perf_counter()
-    S4 = S2.union(S3)
-
-    end = perf_counter()
-    print(end - start)
 #
-    start = perf_counter()
-    S4 = S3.difference(S2)
-
-    end = perf_counter()
-    print(end - start)
-
-    string4='ABCDEF'
-    string5='ABC'
-    S4 = PowerSet()
-    S5 = PowerSet()
-    for s in string4:
-        S4.put(s)
-    for s in string5:
-        S5.put(s)
-
-    assert S4.issubset(S5) == True
-    assert S5.issubset(S4) == False
-
-    S1=PowerSet()
-    S2=PowerSet()
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt')
-    list_of_words=r.data.decode('utf-8').split('\n')
-    limit=20000
-    for w in list_of_words:
-        if limit < 0:
-            break
-        if limit < 10000:
-            S1.put(w)
-        else:
-            S2.put(w)
-        limit-=1
-
-
-
-    start = perf_counter()
-    S3 = S1.union(S2)
-    print(S3.elements)
-    end = perf_counter()
-    time_perf=end-start
-    print(f"Union time is {time_perf}")
-    print(f"The size of S3 is {S3.size()}")
-    print(f"The size of S1 is {S1.size()}")
-    print(f"The size of S2 is {S2.size()}")
+#     start = perf_counter()
+#     S3 = S1.union(S2)
+#     print(S3.elements)
+#     end = perf_counter()
+#     time_perf=end-start
+#     print(f"Union time is {time_perf}")
+#     print(f"The size of S3 is {S3.size()}")
+#     print(f"The size of S1 is {S1.size()}")
+#     print(f"The size of S2 is {S2.size()}")
